4167494cb50ac121f1fa40c67cb73a9505afa148
niv b <nivb52@gmail.com>
2020-11-01 00:42:34 +0200

    adding proxy example

--- proxy.js
+++ proxy.js
@@ -15,7 +15,40 @@ function CryptoCurrencyApi() {
   };
 }
 
-const api = new CryptoCurrencyApi();
+let res;
 
-let res = api.getValue('bitcoin');
+// ====================OLD WAY========================================
+const api = new CryptoCurrencyApi();
+res = api.getValue('bitcoin');
+// console.info(res);
+
+// ====================NEW WAY========================================
+// adding the proxy pattern
+function CryptoCurrencyProxyPattern() {
+  this.api = new CryptoCurrencyApi();
+
+  this.cache = new Map();
+
+  this.getValue = (coin) => {
+    if (this.cache.get(coin)) return this.cache.get(coin);
+    else {
+      let value = api.getValue(coin);
+      this.cache.set(coin, value);
+      console.log('cache ... ', this.cache);
+      return value;
+    }
+  };
+}
+
+const proxy_api = new CryptoCurrencyProxyPattern();
+
+res = proxy_api.getValue('bitcoin');
+console.log(res);
+res = proxy_api.getValue('litecoin');
+console.log(res);
+res = proxy_api.getValue('ethereum');
+console.log(res);
+res = proxy_api.getValue('somecoin');
+console.log(res);
+res = proxy_api.getValue('bitcoin');
 console.log(res);
